bb311f37c081ea0413bc44b3ba390892c24df332
Dani-1138 <danieldemeke92@gmail.com>
2022-12-08 11:47:05 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
  * main - main block
- * return: 0
+ *
+ * Return: always(0) (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
