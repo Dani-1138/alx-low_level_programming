1d66418509e3152c8d7b07ab2c792e7813867454
Dani-1138 <danieldemeke92@gmail.com>
2022-12-09 07:17:06 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - main block
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 0) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
