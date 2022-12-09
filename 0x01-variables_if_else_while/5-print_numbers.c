2253d5f9d72341595c555e9490f59f9349a1c163
Dani-1138 <danieldemeke92@gmail.com>
2022-12-09 05:38:22 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
