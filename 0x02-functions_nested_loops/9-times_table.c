95f24fbf4aa8bf4bf8547d508581a8e3c5911c01
Dani-1138 <danieldemeke92@gmail.com>
2022-12-13 10:20:59 -0800

    mul table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -36,7 +36,7 @@ void times_table(void)
 			else
 			{
 				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '+');
+				_putchar((prod % 10) + '0');
 				if (mul < 9)
 				{
 					_putchar(',');
