c6c702be00eaad31f9ad57e419b7dc9666d99792
Dani-1138 <danieldemeke92@gmail.com>
2022-12-13 03:54:46 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * print_alphabet - entry block
+ *
+ * Return: Always 0 (success)
+ */
+
+int print_alphabet(void)
+{
+	char n;
+
+	n='a';
+	while (n<'z') 
+	{
+			_putchar(n);
+			c++;
+	}
+	_ptchar('\n');
+}
