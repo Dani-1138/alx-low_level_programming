fe8accd8ac92debf2e91a8291f70441b86a1f582
Dani-1138 <danieldemeke92@gmail.com>
2022-12-13 05:37:40 -0800

    print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,14 +10,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return ('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		return ('0');
+		return (0);
 	}
 	else
 	{
-		return ("-1");
+		return (-1);
 	}
 }
