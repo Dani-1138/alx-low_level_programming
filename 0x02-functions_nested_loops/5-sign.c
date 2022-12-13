8ca9aaf55d727adb603aa806a67b3c9603f555bd
Dani-1138 <danieldemeke92@gmail.com>
2022-12-13 05:35:20 -0800

    return sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - code block
+ * @n:number  argument
+ * Return: return 1 , 0 or -1 depend on argument
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return ('+');
+	}
+	else if (n == 0)
+	{
+		return ('0');
+	}
+	else
+	{
+		return ("-1");
+	}
+}
