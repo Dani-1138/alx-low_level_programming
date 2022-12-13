8d0b76d192acc660b1343b2896a9a5e1c730ab6c
Dani-1138 <danieldemeke92@gmail.com>
2022-12-13 07:24:08 -0800

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,6 +7,7 @@
 void jack_bauer(void)
 {
 	int i, j;
+
 	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 60; j++)
@@ -29,7 +30,7 @@ void jack_bauer(void)
 			}
 			else if (j >= 10)
 			{
-                                _putchar(':');
+			       	_putchar(':');
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
