dda98ee772d7c29a1586b1567b6abdbe0e3f89a6
Dani-1138 <danieldemeke92@gmail.com>
2022-12-15 11:01:41 -0800

    is upper case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,4 @@
-/*
- * File: 0-isupper.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _isupper - Checks for uppercase characters.
@@ -11,6 +6,7 @@
  *
  * Return: 1 if character is uppercase, 0 otherwise.
  */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
